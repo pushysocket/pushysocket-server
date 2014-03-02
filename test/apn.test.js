81b89f4e943ffefcc9f6cb5970b99aa7b003e70d
marty <marty.nelson@yahoo.com>
2014-03-01 21:56:50 -0800

    fix apn tests

--- test/apn.test.js
+++ test/apn.test.js
@@ -3,64 +3,51 @@ var assert = require('chai').assert,
 	device = '<a3128b5b 925cec91 978e85d7 b47651f7 1d21faad 638809b7 80b81c15 6d4040a5>'
 
 describe('apn service factory', function(){
-	var apnagent
-	it('connects and returns agent', function(done){
-		apn.create(function(err, agent){
-			assert.notOk(err)
-			assert.ok(agent, 'returns agent')
-			apnagent = agent
-			assert.isFalse(agent.enabled('sandbox'))
-			done()
-		})
-	})
-	it('sends message', function(){
-		var message = 'This is the message'
-		apnagent.on('message:mock', function(body){
-			assert.equal(body, message)
-			done()
-		})
-		apnagent.createMessage()
-			.device(device)
-			.alert(message)
-			.send()
-	})
-	it('sends second message', function(){
-		var message = 'Second message'
-		apnagent.on('message:mock', function(body){
-			assert.equal(body, message)
-			done()
-		})
-		apnagent.createMessage()
-			.device(device)
-			.alert(message)
-			.send()
-	})
-	
-	it('can create second instance', function(done){
-		apn.create(function(err, agent){
-			assert.notOk(err)
-			assert.ok(agent, 'returns agent')
-			assert.isFalse(agent.enabled('sandbox'))
-			
-			/*
-			apnagent.on('message:mock', function(){
-				throw "Should not receive message"
-			})
-			*/
-
-			agent.on('message:mock', function(body){
-				assert.equal(body, message)
+	describe('first agent', function(){
+		var apnagent
+		it('connects and returns agent', function(done){
+			apn.create(function(err, agent){
+				assert.notOk(err)
+				assert.ok(agent, 'returns agent')
+				apnagent = agent
+				assert.isFalse(agent.enabled('sandbox'))
 				done()
-			})	
-
-			var message = 'Second message'
-			agent.createMessage()
+			})
+		})
+		it('sends message', function(done){
+			var message = 'This is the message'
+			apnagent.on('mock:message', function(body){
+				assert.equal(body.payload.aps.alert, message)
+				done()
+			})
+			apnagent.createMessage()
 				.device(device)
 				.alert(message)
 				.send()
 		})
+	})
+	describe('second agent', function(){
+		it('can send message', function(done){
+			apn.create(function(err, agent){
+				assert.notOk(err)
+				assert.ok(agent, 'returns agent')
+				assert.isFalse(agent.enabled('sandbox'))
+				
+				var message = 'Second message'
+
+				agent.on('mock:message', function(body){
+				assert.equal(body.payload.aps.alert, message)
+					done()
+				})	
+
+				agent.createMessage()
+					.device(device)
+					.alert(message)
+					.send()
+			})
 
 
+		})
 	})
 
 })
